a8b28478e24f90215d42317234dd6f279cb024a8
ishanhingle <134205529+ishanhingle@users.noreply.github.com>
2023-08-03 22:55:44 +0530

    search filter added

---
+++ backend/utils/ApiFeatures.js
@@ -0,0 +1,17 @@
+class ApiFeatures{
+    constructor(query,queryString) {
+        this.query=query;
+        this.queryString=queryString;
+    }
+    search(){
+        const keyword=this.queryString ?{
+            name:{
+                $regex:this.queryString,
+                $options:"i",
+            }
+        }:{}
+        this.query=this.query.find({...keyword})
+        return this;
+    }
+}
+module.exports=ApiFeatures;
