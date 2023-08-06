2ae7cd26335f5c9152458509438f17d89764ddd3
ishanhingle <134205529+ishanhingle@users.noreply.github.com>
2023-08-06 22:11:27 +0530

    pagination added

--- backend/utils/ApiFeatures.js
+++ backend/utils/ApiFeatures.js
@@ -1,17 +1,45 @@
-class ApiFeatures{
-    constructor(query,queryString) {
-        this.query=query;
-        this.queryString=queryString;
+class ApiFeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
     }
-    search(){
-        const keyword=this.queryString ?{
-            name:{
-                $regex:this.queryString,
-                $options:"i",
+    search() {
+        const keyword = this.queryString.name ? {
+            name: {
+                $regex: this.queryString.name,
+                $options: "i",
             }
-        }:{}
-        this.query=this.query.find({...keyword})
+        } : {}
+        this.query = this.query.find({ ...keyword })
         return this;
     }
+    filter() {
+        const filterString = { ...this.queryString };
+        const removeFields = ["name", "page", "limit"];
+        removeFields.forEach(field => delete filterString[field]);
+
+             //to change gte to $gte for price and rating
+             if (filterString.price) {
+                 filterString.price = {
+                     $gte: Number(filterString.price.gte) || 0,
+                     $lte: Number(filterString.price.lte) || 10000000000000
+                 }
+             }
+             if (filterString.rating) {
+                 filterString.rating = {
+                     $gte:Number(filterString.rating.gte)|| 0
+                 }
+             }
+
+        this.query = this.query.find(filterString);
+        return this;
+    }
+    pagination(maxPerPage){
+        const currPage=Number(this.queryString.page) || 1
+        const skip=maxPerPage*(currPage-1);
+        this.query=this.query.limit(maxPerPage).skip(skip);
+        return this
+    }
+
 }
-module.exports=ApiFeatures;
+module.exports = ApiFeatures;
